bb027e6ca13d73da587dcbe2d89d83ad2b12b56f
Sebastian <smcasanova17@hotmail.com>
2022-11-26 08:42:08 -0500

    first commit

---
+++ Develop/index.js
@@ -0,0 +1,38 @@
+// TODO: Include packages needed for this application
+const { writeFile } = require('fs').promises;
+const inquirer = require('inquirer')
+const generateMarkdown = require('./utils/generateMarkdown')
+// TODO: Create an array of questions for user input
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+        type:'input',
+        name: 'projectName',
+        message: 'What is your name?'
+    },
+        {
+        type:'input',
+        name: 'projectName',
+        message: 'What is your name?'
+    },
+        {
+        type:'input',
+        name: 'projectName',
+        message: 'What is your name?'
+    },
+        {
+        type:'input',
+        name: 'projectName',
+        message: 'What is your name?'
+    },
+    ])
+}
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
